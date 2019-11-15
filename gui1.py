e5bc8a4050bba1b73ada8a8c3f2ea792a2b2d603
viethung98 <viethung.cse@gmail.com>
2019-11-15 10:26:03 +0700

    set default button

--- gui1.py
+++ gui1.py
@@ -2,25 +2,25 @@ import PySimpleGUI as sg
 
 
 layout = [
-
     [sg.Checkbox('Windows 7', enable_events=True, key='win7', size=(15, 10)),
      sg.Text('Numbers of VMs'),
-     sg.Spin([1, 2, 3, 4], initial_value=1, key='num_win7'),
-     sg.Radio("Default", "mode1", default=True), sg.Radio("Custom", "mode1")],
-    [sg.Checkbox('Windows 10', enable_events=True, key='win10', size=(15, 10)), sg.Text(
-        'Numbers of VMs'), sg.Spin([1, 2, 3, 4], enable_events=True ,initial_value=1, key='num_win10'),
-     sg.Radio("Default", "mode2", default=True), sg.Radio("Custom", "mode2")],
-    [sg.Checkbox('Windows Server',  key='winsv', size=(15, 10)), sg.Text(
-        'Numbers of VMs'), sg.Spin([1, 2, 3, 4], initial_value=1, key='num_winsv'),
-     sg.Radio("Default", "mode3", default=True), sg.Radio("Custom", "mode3")],
-    [sg.Submit('Next', key='btn_next')]
+     sg.Spin([1, 2, 3, 4], initial_value=1, key='num_win7')],
+    #  sg.Radio("Default", "mode1", default=True, key='default_win7'), 
+    #  sg.Radio("Custom", "mode1", key='custom_win7'))],
+    [sg.Checkbox('Windows 10', enable_events=True, key='win10', size=(15, 10)), 
+     sg.Text('Numbers of VMs'),
+     sg.Spin([1, 2, 3, 4], enable_events=True ,initial_value=1, key='num_win10')],
+    [sg.Checkbox('Windows Server',  key='winsv', size=(15, 10)), 
+     sg.Text('Numbers of VMs'), 
+     sg.Spin([1, 2, 3, 4], initial_value=1, key='num_winsv')],
+    [sg.Submit('Default', key='btn_default'), 
+     sg.Submit('Next', key='btn_next')]
 ]
 
 
 def create(scr):
     window = sg.Window('Config', layout)
     return window
-
 
 def destroy():
     window.Close()
--- main.py
+++ main.py
@@ -1,54 +1,60 @@
 import gui_Factory as fac 
 
-
-scr = []
 def appear(win):
     while True:
-        event, values= win.Read()
-        win.Values=values
+        event, values = win.Read()
+        win.Values = values
         if event is None:            
             return 0
-        elif event=='btn_next':
+        elif event == 'btn_next':
             return 1
-        elif event=='btn_prev':
+        elif event == 'btn_prev':
             return -1
-       
-            
+        elif event == 'btn_default':
+            # win.FindElement('win' + ).Update(disabled = True)
+            # win.FindElement('win10').Update(disabled = True)
+            parent_keys = list(win.AllKeysDict.keys())
+            for key in parent_keys:
+                win.FindElement(key).Update(disabled=True)
+            print(parent_keys)
 
+       
 def disappear(win):
     win.Hide()
 
-
+scr = []
 pos=0
-scr.append(fac.createGui(pos,scr))
+scr.append(fac.createGui(pos, scr))
 
 while 1:
     additive=0
-    
-    
-    result=appear(scr[pos])
-    print(scr[0].Values)
+    result = appear(scr[pos])
+    # print(scr[0].Values)
+
     if result == 0:
         break
-    elif result==1:
-        
-        if pos+1 < len(scr):
-            scr[pos+1].UnHide()
-        else: 
-            scr.append(fac.createGui(pos+1,scr))  
+    elif result == 1:
+        #the first screen is Special-> create VM
+        if pos == 0:
+            if pos+1 < len(scr):
+                scr[pos+1] = fac.createGui(pos + 1, scr)
+            else: 
+                scr.append(fac.createGui(pos + 1, scr))  
+        #Others screen
+        else:
+            if pos+1 < len(scr):
+                scr[pos+1].UnHide()
+            else: 
+                scr.append(fac.createGui(pos + 1,scr))  
         scr[pos].Hide()   
         additive=1
-    elif result==-1:  
+    elif result == -1:  
         scr[pos].Hide()
         scr[pos-1].UnHide()   
-        additive=-1
+        additive = -1
 
     pos+=additive
    
 
 for element in scr:
     element.Close()
-
-    
-
-
